e22c53a793ec03b86f0f30f2204e6934ed36a788
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-02-07 12:50:04 +0800

    import yii2

---
+++ models/User.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $ID
+ * @property string $username
+ * @property string $password
+ * @property string $authKey
+ * @property string $accessToken
+ */
+class User extends \yii\db\ActiveRecord implements IdentityInterface
+{
+    public $confirm_password;
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'authKey', 'accessToken'], 'required'],
+            [['username', 'password', 'authKey', 'accessToken'], 'string', 'max' => 255],
+            ['password', 'string', 'min' => 8],
+            [['confirm_password'], 'compare', 'compareAttribute' => 'password', 'message' => 'Passwords do not match'],
+        ];
+    }
+    public function signup()
+    {
+        $user = new User();
+        $user->username = $this->username;
+        $user->password = \Yii::$app->security->generatePasswordHash($this->password);
+        $user->accessToken = \Yii::$app->security->generateRandomString();
+        $user->authKey = \Yii::$app->security->generateRandomString();
+
+        if ($user->save()){
+            return true;
+        }
+
+        \Yii::error("Uncessfull account creation". VarDumper::dumpAsString($user->errors));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'ID' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'authKey' => 'Auth Key',
+            'accessToken' => 'Access Token',
+        ];
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return self::find()->where(['accessToken' => $token])->one();
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return self::findOne(['username' => $username]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->ID;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+}
